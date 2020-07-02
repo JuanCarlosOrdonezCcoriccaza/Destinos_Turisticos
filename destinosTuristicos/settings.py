d3a8b32a72dbc4076e3d319817c785dc4735136d
JuanCarlosOrdCc <jordonezcc@unsa.edu.pe>
2020-07-02 13:27:00 -0500

    V1.0.8 sqlite

--- destinosTuristicos/settings.py
+++ destinosTuristicos/settings.py
@@ -76,6 +76,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'USER': 'SQLite',
+        #'PASSWORD':'SQLite',
+        #'HOST':'localhost' 
     }
 }
 
