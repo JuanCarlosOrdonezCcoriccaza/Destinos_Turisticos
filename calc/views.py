2da9bf04505a1ab69e6fdbb2bb05ab9501e78500
JuanCarlosOrdCc <jordonezcc@unsa.edu.pe>
2020-07-02 03:46:33 -0500

    V1.0.3

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return render( request ,'home.html',{'name':'Juan'})
+
+def add(request):
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    resultado = val1 + val2 
+    return render(request ,"result.html",{'resultado':resultado})
