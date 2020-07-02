3d8dead5deffc18f3f5d0f190d7e32fa663c6de4
JuanCarlosOrdCc <jordonezcc@unsa.edu.pe>
2020-07-02 04:26:10 -0500

    V1.0.5 Statics

--- destinosTuristicos/urls.py
+++ destinosTuristicos/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('',include('calc.urls')),
+    path('travello',include('travello.urls')),
     path('admin/', admin.site.urls),
 ]
